96dca897398ce4400275755fe1ab430feb5596a2
Eric Park <me@ericswpark.com>
2024-09-22 19:10:09 -0400

    Rewrite Q1 algo
    To make it more pseudocode-friendly and easier (?) to read

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,17 @@ pub fn valid_tours(b: &[u32]) -> u32 {
     let mut t = Vec::new();
 
     for (day, _rest_days_needed) in b.iter().enumerate() {
-        if day == 0 {
-            t.push(1);
+        t.push(1);
+
+        if day == 0 {   // Base case
             continue;
         }
 
-        let mut acc = 1;
-
         for prev_days in 0..day {
             if b[prev_days] + (prev_days as u32) < day as u32 {
-                acc += t[prev_days];
+                t[day] += t[prev_days];
             }
         }
-        t.push(acc);
     }
 
     t.iter().sum()
